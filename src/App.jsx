4458cec58417b243db3ac47118443215ae469347
sailecodes <eliasiv.codes@gmail.com>
2024-07-24 15:21:47 -0700

    build

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@ import axiosc from "./utilities/axios";
 import { gql, useMutation, useQuery } from "@apollo/client";
 
 function App() {
-  // const { loading, error, data } = useQuery(gql`
-  //   query {
-  //     test {
-  //       dummy_attr
-  //     }
+  const { loading, error, data } = useQuery(
+    gql`
+      query {
+        test {
+          dummy_attr
+        }
+      }
+    `,
+    {
+      onCompleted: (data) => {
+        console.log("Query successful");
+        console.log(data);
+      },
+    }
+  );
+
+  // const [mutate, { loading, error, data }] = useMutation(gql`
+  //   mutation {
+  //     mutationTest
   //   }
   // `);
-
-  const [mutate, { loading, error, data }] = useMutation(gql`
-    mutation {
-      mutationTest
-    }
-  `);
 
   const handleREST = async () => {
     const response = await axiosc.get("/test");
@@ -22,10 +30,10 @@ function App() {
   };
 
   const handleGQLMutation = async () => {
-    await mutate();
+    // await mutate();
   };
 
-  if (!loading && !error && data) console.log(data);
+  // if (!loading && !error && data) console.log(data);
 
   return (
     <>
