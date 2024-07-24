b753dd3f1a71a2a36068b777aa1373dc180ec52b
sailecodes <eliasiv.codes@gmail.com>
2024-07-24 11:44:32 -0700

    build

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5200/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+
+  // NOTE: Only needed for REST via Axios
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:5200/api",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
 });
