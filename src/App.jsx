4b629397e85f49c1e7873efbbbd8df3a0de9c28c
sailecodes <eliasiv.codes@gmail.com>
2024-07-24 15:17:25 -0700

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import axiosc from "./utilities/axios";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 
 function App() {
-  const { loading, error, data } = useQuery(gql`
-    query {
-      test {
-        dummy_attr
-      }
+  // const { loading, error, data } = useQuery(gql`
+  //   query {
+  //     test {
+  //       dummy_attr
+  //     }
+  //   }
+  // `);
+
+  const [mutate, { loading, error, data }] = useMutation(gql`
+    mutation {
+      mutationTest
     }
   `);
 
@@ -15,9 +21,18 @@ function App() {
     console.log(response.data);
   };
 
+  const handleGQLMutation = async () => {
+    await mutate();
+  };
+
   if (!loading && !error && data) console.log(data);
 
-  return <button onClick={handleREST}>REST Button</button>;
+  return (
+    <>
+      <button onClick={handleREST}>REST Button</button>
+      <button onClick={handleGQLMutation}>GQL Mutate Button</button>
+    </>
+  );
 }
 
 export default App;
