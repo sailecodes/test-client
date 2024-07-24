e86a5c76c545ae0b179f46edfbee33445e5f6399
sailecodes <eliasiv.codes@gmail.com>
2024-07-24 15:36:38 -0700

    build

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client";
-
-const httpLink = createHttpLink({
-  uri: "https://saile-test-server.up.railway.app",
-});
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
   // uri: "http://localhost:5200",
-  uri: httpLink,
+  uri: "https://saile-test-server.up.railway.app",
   cache: new InMemoryCache(),
   credentials: "include",
 });
