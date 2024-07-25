26354f2c1a6e517a2c9efecbdf83ba5d64544f17
sailecodes <eliasiv.codes@gmail.com>
2024-07-25 13:25:23 -0700

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import axiosc from "./utilities/axios";
-import { gql, useMutation, useQuery } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 
 function App() {
   const { loading, error, data } = useQuery(
@@ -18,27 +18,14 @@ function App() {
     }
   );
 
-  // const [mutate, { loading, error, data }] = useMutation(gql`
-  //   mutation {
-  //     mutationTest
-  //   }
-  // `);
-
   const handleREST = async () => {
     const response = await axiosc.get("/test");
     console.log(response.data);
   };
 
-  const handleGQLMutation = async () => {
-    // await mutate();
-  };
-
-  // if (!loading && !error && data) console.log(data);
-
   return (
     <>
       <button onClick={handleREST}>REST Button</button>
-      <button onClick={handleGQLMutation}>GQL Mutate Button</button>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // NOTE: Only needed for REST via Axios
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:5200/api",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5200/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
